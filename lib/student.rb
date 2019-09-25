e210b3d66991f978a625b36fb29d4273104c1fe7
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-25 14:08:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,8 +38,10 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create
-
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    Student.save
+    Student
   end
 
 end
